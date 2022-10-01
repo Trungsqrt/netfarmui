ab22e7941a16d56e9b75da410314ea0f1780c503
unknown <nguyennhumay901@gmail.com>
2022-10-01 19:43:33 +0700

    login form + responsive + validation

---
+++ src/components/Login.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styles from "../components/Login.module.css";
+import loginImage from "../assets/image/loginImage.jpg";
+function login() {
+   return (
+      <section className={styles.container}>
+         {/* Left */}
+         <div className={styles.left}>
+            <img src={loginImage} alt="" className={styles.loginImage} />
+         </div>
+         {/* Right */}
+         <div className={styles.right}>
+            <div className={styles.loginForm}>
+               {/* Label */}
+               <div className={styles.label}>
+                  <h5 className={styles.title}>Đăng nhập</h5>
+                  <p className={(styles.title, styles.titleDesc)}>
+                     Nhập tên tài khoản hoặc số điện thoại và mật khẩu
+                  </p>
+               </div>
+               {/* Form */}
+               <form>
+                  {/* usn */}
+                  <div className={styles.inputContainer}>
+                     <input
+                        type="text"
+                        name="uname"
+                        required
+                        minLength={6}
+                        placeholder="Tài khoản / Số điện thoại"
+                        className={styles.inputField}
+                     />
+                     <i class="iconAuth fa-solid fa-user"></i>
+                  </div>
+                  {/* psw */}
+                  <div className={styles.inputContainer}>
+                     <input
+                        type="password"
+                        name="pass"
+                        required
+                        minLength={6}
+                        placeholder="Mật khẩu"
+                        className={styles.inputField}
+                     />
+                     <i class="iconAuth fa-solid fa-lock"></i>
+                  </div>
+                  {/* forgot password */}
+                  <div className={styles.forgotSection}>
+                     <a href="#" className={styles.forgot}>
+                        Quên mật khẩu
+                     </a>
+                  </div>
+                  {/* btn */}
+                  <div>
+                     <input
+                        type="submit"
+                        className="btnSubmit"
+                        value="Đăng nhập"
+                     />
+                  </div>
+               </form>
+
+               <div className={styles.line}></div>
+
+               <div className={styles.createNew}>
+                  <div className={styles.createNewSection}>
+                     <input
+                        type="submit"
+                        value="Đăng ký"
+                        className={styles.createNewBtn}
+                     />
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   );
+}
+
+export default login;
