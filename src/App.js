e18fbbf4af64c34abc46cec064950102adde10df
unknown <nguyennhumay901@gmail.com>
2022-10-14 21:34:09 +0700

    merge

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import Home from "./components/home/Homepage";
 import Personal from "./components/personalinfo/Personal";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-<<<<<<< HEAD
-import Editor from "./components/editor/Editor";
-import CreateExpert from "./components/createExpert/CreateExpert";
-=======
-import EditorArticle from "./components/editor/EditorArticle";
 
->>>>>>> a5aaee3 (login)
+import CreateExpert from "./components/createExpert/CreateExpert";
+
 function App() {
    return (
       <div className="App">
@@ -18,12 +14,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/personal" element={<Personal />} />
-<<<<<<< HEAD
-            <Route path="/editor" element={<Editor />} />
-            <Route path="/create" element={<CreateExpert />}/>
-=======
-            <Route path="/editor" element={<EditorArticle />} />
->>>>>>> a5aaee3 (login)
+            <Route path="/createexpert" element={<CreateExpert />} />
          </Routes>
       </div>
    );
