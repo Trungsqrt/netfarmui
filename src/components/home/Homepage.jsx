70f3e57282a5e324abe704459bd7db64858b57e6
VietAnh188 <anhkun123456@gmail.com>
2022-10-16 12:59:36 +0700

    creating slide component

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+import Slide from "../slide/Slide";
 import Footer from "../footer/Footer";
+
 function Homepage() {
    return (
       <div>
          <Navbar />
+         <Slide/>
          <Footer />
       </div>
    );
