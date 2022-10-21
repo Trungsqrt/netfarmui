318df76a5fd9f4ff77f96f2c946dcba788b67d39
unknown <nguyennhumay901@gmail.com>
2022-10-21 09:30:03 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import Login from "./components/login/Login";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/home/Homepage";
-import Personal from "./components/personalinfo/Personal";
-import ArticleHandler from "./components/articleHandler/ArticleHandler";
-import AdminPage from "./components/adminPage/AdminPage";
-import CreateExpert from "./components/createExpert/CreateExpert";
-import CalendarHandler from "./components/calendarHandler/CalendarHandler";
-import ArticleView from "./components/articleView/ArticleView";
+import Login from './components/login/Login';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Homepage';
+import Personal from './components/personalinfo/Personal';
+import ArticleHandler from './components/articleHandler/ArticleHandler';
+import AdminPage from './components/adminPage/AdminPage';
+import CreateExpert from './components/createExpert/CreateExpert';
+import CalendarHandler from './components/calendarHandler/CalendarHandler';
+import ArticleView from './components/articleView/ArticleView';
 import ClientHome from './components/client_farm/home/ClientHome';
 import DetailArticle from './components/client_farm/detail/DetailArticle';
 import InforPage from './components/client_farm/infor/InforPage';
-                
+
 function App() {
-   return (
-      <div className="App">
-         <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/personal" element={<Personal />} />
-            <Route path="/createexpert" element={<CreateExpert />} />
-            <Route path="/articlehandler" element={<ArticleHandler />} />
-            <Route path="/calenderhandler" element={<CalendarHandler />} />
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="/articleView" element={<ArticleView />} />
-            <Route path="/client/home" element={<ClientHome />} />
-            <Route path="/detail/:id" element={<DetailArticle />} />
-            <Route path="/inforPage" element={<InforPage />} />
-         </Routes>
-      </div>
-   );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/personal" element={<Personal />} />
+                <Route path="/createexpert" element={<CreateExpert />} />
+                <Route path="/articlehandler" element={<ArticleHandler />} />
+                <Route path="/calenderhandler" element={<CalendarHandler />} />
+                <Route path="/admin" element={<AdminPage />} />
+                <Route path="/articleView" element={<ArticleView />} />
+                <Route path="/client/home" element={<ClientHome />} />
+                <Route path="/detail/:id" element={<DetailArticle />} />
+                <Route path="/inforPage" element={<InforPage />} />
+            </Routes>
+        </div>
+    );
 }
-export default App
+export default App;
--- src/components/slide/images.js
+++ src/components/slide/images.js
@@ -1,8 +1,10 @@
-export default [
-    "https://bom.so/YOKHbx",
-    "https://bom.so/mok5fo",
-    "https://bom.so/aoIlEi",
-    "https://bom.so/M37UHc",
-    "https://bom.so/q678i6",
-    "https://bom.so/YOKHbx"
-  ];
+const images = [
+   "https://bom.so/YOKHbx",
+   "https://bom.so/mok5fo",
+   "https://bom.so/aoIlEi",
+   "https://bom.so/M37UHc",
+   "https://bom.so/q678i6",
+   "https://bom.so/YOKHbx",
+];
+
+export default images;
