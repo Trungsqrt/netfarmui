13163b8a69b88aa2e31188b3a957b9cbf938ccd6
BaoQuyen <thanhlacduong2k@gmail.com>
2022-10-22 19:11:30 +0700

    update view product

--- src/components/client_farm/e_commerce/Shop/Shop.jsx
+++ src/components/client_farm/e_commerce/Shop/Shop.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Products from './Products/Products';
 import Category from './Category/Category';
+import Header from '../../share/header/Header';
 const Shop = () => {
     return (
-        <div className="shop">
-            <Category></Category>
-            <Products></Products>
+        <div>
+            <Header></Header>
+            <div className="shop">
+                <Category></Category>
+                <Products></Products>
+            </div>
         </div>
     );
 };
