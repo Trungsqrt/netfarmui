1d753cb082c9ca8b48b0740e0873fa2a69d313e9
BaoQuyen <thanhlacduong2k@gmail.com>
2022-11-25 15:27:30 +0700

    dscx

--- src/components/client_farm/infor/DSS/TemperatureChart.jsx
+++ src/components/client_farm/infor/DSS/TemperatureChart.jsx
@@ -27,16 +27,18 @@ const TemperatureChart = () => {
 
     return (
         <div className="App">
-            <ComposedChart width={730} height={250} data={data}>
-                <XAxis dataKey="Ngày" />
-                <YAxis yAxisId={1} orientation="right" label={{ value: 'Lượng mưa mm', angle: -90 }} />
-                <YAxis yAxisId={2} label={{ value: 'Nhiệt độ (oC)', angle: -90 }} />
-                <Tooltip />
-                <Legend />
-                <CartesianGrid stroke="#f5f5f5" />
-                <Bar yAxisId={1} dataKey="LuongMua" barSize={40} fill="#413ea0" />
-                <Line yAxisId={2} type="monotone" dataKey="NhietDo" stroke="#ff0000" />
-            </ComposedChart>
+            <ResponsiveContainer>
+                <ComposedChart width={730} height={250} data={data}>
+                    <XAxis dataKey="Ngày" />
+                    <YAxis yAxisId={1} orientation="right" label={{ value: 'Lượng mưa mm', angle: -90 }} />
+                    <YAxis yAxisId={2} label={{ value: 'Nhiệt độ (oC)', angle: -90 }} />
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Bar yAxisId={1} dataKey="LuongMua" barSize={40} fill="#413ea0" />
+                    <Line yAxisId={2} type="monotone" dataKey="NhietDo" stroke="#ff0000" />
+                </ComposedChart>
+            </ResponsiveContainer>
         </div>
     );
 };
