4ae7428307855f5dba613d5a91dc403865da8c2f
hnthinh55 <hnthinh55@gmail.com>
2023-01-02 22:31:49 +0700

    fix 404

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ import ArticleHandler from './components/articleHandler/ArticleHandler';
 import AdminPage from './components/adminPage/AdminPage';
 import ExpertPage from './components/expertPage/ExpertPage';
 import CreateExpert from './components/admin_farm/ManageUser/Expert/CreateExpert';
-// import CalendarHandler from './components/calendarHandler/CalendarHandler';
 import AddSchedule from './components/admin_farm/ManagePost/Addschedule';
 import ClientHome from './components/client_farm/home/ClientHome';
 import DetailArticle from './components/client_farm/detail/DetailArticle';
-// import InforPage from './components/client_farm/infor/InforPage';
-// import Policy from './components/client_farm/infor/policy/Policy';
-// import Manual from './components/client_farm/infor/manual/Manual';
-// import News from './components/client_farm/infor/news/News';
-// import Schedule from './components/client_farm/infor/schedule/Schedule';
 import Shop from './components/client_farm/e_commerce/Shop/Shop';
 import ProductDetail from './components/client_farm/e_commerce/Shop/Product/ProductDetail';
 import Cart from './components/client_farm/e_commerce/Cart/Cart';
@@ -51,11 +45,6 @@ function App() {
                 <Route path="/detail/:id" element={<DetailArticle />} />
                 <Route path="/editArticle/:id" element={<ArticleHandler />} />
                 <Route path="/editSchedule/:id" element={<AddSchedule />} />
-                {/* <Route path="/inforPage" element={<InforPage />} />
-                <Route path="/inforPage/policy" element={<Policy />} />
-                <Route path="/inforPage/manual" element={<Manual />} />
-                <Route path="/inforPage/news" element={<News />} />
-                {/* <Route path="/inforPage/schedule" element={<Schedule />} /> */}
                 <Route path="/shop" element={<Shop />} />
                 <Route path="/shop/product/detail/:id" element={<ProductDetail />} />
                 <Route path="/shop/cart" element={<Cart />} />
